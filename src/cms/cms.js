0210d5278375deed9d5894dff547c2f2261a7cdc
deegha <shuboothi@gmail.com>
2019-10-23 18:21:53 +0530

    vids

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -11,6 +11,7 @@ CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('about2', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('vid', BlogPostPreview)
